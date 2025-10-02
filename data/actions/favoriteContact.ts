d5a4d7ed9b78cb1690e145880953050e2f42915a
Aurora Scharff <aurora.sofie@gmail.com>
2025-10-02 13:16:01 +0200

    Add favorite

---
+++ data/actions/favoriteContact.ts
@@ -0,0 +1,20 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
+
+export async function favoriteContact(contactId: string, favorite: boolean) {
+  await slow();
+
+  await prisma.contact.update({
+    data: {
+      favorite: !favorite,
+    },
+    where: {
+      id: contactId,
+    },
+  });
+
+  revalidatePath('/');
+}
