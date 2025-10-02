09772386b1d210c2a3171dba4e0d3bc5fe73e927
Aurora Scharff <aurora.sofie@gmail.com>
2025-10-02 12:48:49 +0200

    Update

---
+++ data/actions/createEmptyContact.ts
@@ -0,0 +1,14 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
+
+export async function createEmptyContact() {
+  await slow();
+  const contact = await prisma.contact.create({
+    data: {},
+  });
+
+  redirect(`/contacts/${contact.id}/edit`);
+}
